820820171c937193aa750459a688a57b93e12453
Alexandra Danes <alexdanes@Alexandras-MacBook-Pro-2.local>
2019-04-15 15:42:58 +0100

    View a main menu of options

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,24 +1,34 @@
 package com.twu.biblioteca;
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to biblioteca! Your one-stop-shop for great book titles in Bangalore");
-
-
-        /*ListOfBooks books1= new ListOfBooks();
-        books1.populateListOfBooks();
-        books1.displayListOfBooks();*/
+        System.out.println("\n Welcome to biblioteca! Your one-stop-shop for great book titles in Bangalore \n" );
+        System.out.println("Menu:\n");
+        System.out.println("[1] List of books");
+        System.out.println("[2] Display something else \n");
 
         ListOfBooks book1 = new ListOfBooks ("HEAD FIRST JAVA", "Bert Bate", 2003);
-        book1.displayBook();
-
         ListOfBooks book2 = new ListOfBooks ("TDD BY EXAMPLE", "Kent Beck", 2000);
-        book2.displayBook();
+        ListOfBooks book3 = new ListOfBooks ("CLEAN CODE", "Robert Martin", 2008);
 
-        ListOfBooks book3 = new ListOfBooks ("CLEAN CODE", "Robert Cecil Martin", 2008);
-        book3.displayBook();
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your choice: ");
+        int i = in.nextInt();
+        switch (i) {
+            case 1:
+                book1.displayBook();
+                book2.displayBook();
+                book3.displayBook();
+                break;
+            case 2:
+                System.out.println("something else");
+                break;
+
+        }
 
     }
 }
